2e613fa3214011905606475a731135914812ebb3
Joel <jambranojazc2001@gmail.com>
2021-11-21 00:06:18 -0400

    sell

--- src/index.js
+++ src/index.js
@@ -171,7 +171,7 @@ app.get("/seler",(req,res)=>{
    
     rows.forEach( (elemento,index) => {
       if (!fecha.includes(elemento.time)) {
-        fecha.push(elemento.time);
+        fecha.push(elemento);
         if(index==rows.length-1){
           res.json(fecha)
         }
